a6884565b6160e137452e33218876a8e96d38e71
Pratap <pratapmridha@gmail.com>
2020-10-01 11:23:30 +0530

    Addes Stack with Max

---
+++ Algorithms/13_stack_with_max/11_stack_with_max/stack.py
@@ -0,0 +1,41 @@
+
+"""
+Push O(1) :Push Element To Top
+Pop O(1) : POP Element From Top
+MAX O(n) : Returns max Element from the stack
+Peek O(n): Returns Top Element
+Size O(1) : Returns Size of the stack
+"""
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+    def max(self):
+        return max(self.items)
+
+if __name__ == "__main__":
+    stack=Stack()
+    stack.push(5)
+    stack.push(10)
+    stack.push(50)
+    stack.push(59)
+    stack.push(30)
+    
+    print("Max Element :", stack.max())
+
+    while stack.size()!=0:
+        print(stack.pop())
