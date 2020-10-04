9ebc79cc793b5d246319ba117d0d2d5250744409
Suyash Gupte <gupte.suyash11@gmail.com>
2020-10-04 23:30:22 +0530

    Update prefix.py

--- Algorithms/24_Prefix_Word_Count/prefix.py
+++ Algorithms/24_Prefix_Word_Count/prefix.py
@@ -1,3 +1,5 @@
+
+
 def polyhash(string, p, x):
     val = ord(string[0])
     y = 1
@@ -8,42 +10,26 @@ def polyhash(string, p, x):
         val += y*ord(string[i])
     if(val >= p):
         val = val % p
-    return val, y
-
-
-def pre_compute_hashes(arr, P, T, p, x):
-    H = [None] * (T-P+1)
-    S = arr[T-P:T]
-    H[T - P] ,y = polyhash(S, p, x)
-    y = y*x
-    if(y >= p):
-        y = y % p
-    for i in range(T-P-1, -1, -1):
-        H[i] = (x*H[i+1]) + ord(arr[i]) - (y*ord(arr[i+P]))
-        if(H[i] >= p or H[i] < 0):
-            H[i] = H[i] % p
-    return H
+    return val
 
 def rabin_karp(arr, pattern):
     p = 100000000000007
     x = 3
     P = len(pattern)
-    P_hash, y = polyhash(pattern, p, x)
+    P_hash = polyhash(pattern, p, x)
     result = []
     for text in arr:
         string = list(str(text))
         T = len(string)
         if(string == pattern):
             result.append(text)
-            
         if(P >= T):
             continue
-        H = pre_compute_hashes(string, P, T, p, x)
-        for i in range(T-P+1):
-            if(P_hash != H[i]):
-                continue
-            #if(arr[i:i+P] == pattern):
-            result.append(text)
+        prefix = text[0:P]
+        H  = polyhash(prefix, p, x)
+        if(P_hash != H):
+            continue
+        result.append(text)
     return result
 
 
