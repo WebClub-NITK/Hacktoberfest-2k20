a31c5498a782d96f1d60a0b3e161f7e0cef344b4
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-05 19:59:44 +0530

    Add files via upload

---
+++ Algorithms/70_String_Transformation/stringtrans.cpp
@@ -0,0 +1,52 @@
+ 
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+
+int minimumOps(string& A, string& B) 
+{ 
+    int m = A.length(), n = B.length(); 
+  
+    
+    if (n != m) 
+       return -1; 
+    int count[256]; 
+    memset(count, 0, sizeof(count));
+	
+    for (int i=0; i<n; i++)   
+       count[A[i]]++; 
+    for (int i=0; i<n; i++)   
+       count[B[i]]--;         
+    
+  
+    
+    int res = 0; 
+    for (int i=n-1, j=n-1; i>=0; ) 
+    { 
+        
+        while (i>=0 && A[i] != B[j]) 
+        { 
+            i--; 
+            res++; 
+        } 
+  
+        
+        if (i >= 0) 
+        { 
+            i--; 
+            j--; 
+        } 
+    } 
+    return res; 
+} 
+  
+
+int main() 
+{ 
+    string A ; 
+    string B ;
+    getline(cin,A);
+    getline(cin,B);
+    cout << "Minimum operations required is " << minimumOps(A, B); 
+    return 0; 
+} 
