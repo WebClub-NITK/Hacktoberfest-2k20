0e7d5ac5bd5a802c39ea85430d0a9461b2eb6357
Bhawesh Bhansali <bhansalibhawesh@yahoo.com>
2020-10-12 17:19:31 +0530

    Add scripts to encrypt and decrypt a file using qpdf Closes #38

---
+++ Systems/38_PDF_encryption/decrypt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#usage
+# ./decrypt.sh -i <input_file> -u <user_password>
+#Generates the decrypted file
+
+while getopts i:u: flag
+do
+    case "${flag}" in
+        i) input_file=${OPTARG};;
+        u) user_password=${OPTARG};;
+    esac
+done
+
+filename=$(basename -- "$input_file")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+output_file="${filename}_dec.${extension}"
+
+qpdf --decrypt --password=$user_password $input_file $output_file
+
+if [$? -eq 0]; then
+	echo "Successfully decrypted. Created ${output_file}"
+else
+	echo "Error while decrypting"
+fi
