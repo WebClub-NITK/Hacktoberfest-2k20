b7a7135d3daee9413153c666cbb55a8126ddb894
KangHyeoni <rkdgus0410@naver.com>
2020-10-15 17:47:08 +0900

    solved issue #106

---
+++ Systems/OpenMP Pi/using_openmp.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+static long num_steps = 100000000;
+double step;
+int main()
+{
+    clock_t start = clock();
+    int i;
+    double x, pi, sum = 0.0;
+    step = 1.0/(double)num_steps;
+    #pragma omp parallel for private(x) reduction(+:sum)
+    for (i=0; i<num_steps; i++) {
+        x = (i+0.5)*step;
+        sum = sum + 4.0/(1.0+x*x);
+    }
+    pi = step * sum;
+    printf("pi is approximately : %.6f\n", pi);
+    printf("execution time : %d\n", (int)(clock() - start));
+    return 0;
+}
