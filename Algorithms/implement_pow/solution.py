ecdbbba9a4ba330fdafb504fe92791b34576f318
Ares358 <52101053+Ares358@users.noreply.github.com>
2020-10-27 20:55:43 +0530

    Create solution.py

---
+++ Algorithms/implement_pow/solution.py
@@ -0,0 +1,14 @@
+def power(x, y):
+    if (y == 0): return 1
+    temp = power(x, int(y / 2))
+
+    if (y % 2 == 0):
+        return temp * temp
+    else:
+        if (y > 0):
+            return x * temp * temp
+        else:
+            return (temp * temp) / x
+
+x, y = 2, 10
+print('%.3f' % (power(x, y)))
